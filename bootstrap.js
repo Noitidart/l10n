b3fe295052747f54a3314038bf238d8fffdbdee8
Noitidart <noitidart@gmail.com>
2014-11-29 16:47:05 -0800

    initial.rev3
    initial commit for l10n-html-options, it works

--- bootstrap.js
+++ bootstrap.js
@@ -1,15 +1,7 @@
-const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
-Cu.import('resource://gre/modules/Services.jsm');
-
-var stringBundle = Services.strings.createBundle('chrome://l10n/locale/global.properties?' + Math.random()); // Randomize URI to work around bug 719376
-
 function install() {}
+
 function uninstall() {}
 
-function startup() {
-	var aDOMWindow = Services.wm.getMostRecentWindow('navigator:browser');
-	aDOMWindow.gBrowser.addTab('chrome://l10n/content/my_html.xhtml');
-	aDOMWindow.gBrowser.addTab('chrome://l10n/content/my.xul');
-}
- 
+function startup() {}
+
 function shutdown() {}
