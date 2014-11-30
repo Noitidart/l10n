f35771a25f7cac1c867bfc63a6cfeb73ff3e1a18
Noitidart <noitidart@gmail.com>
2014-11-29 16:48:35 -0800

    initial.rev3
    removed the stringBundle code, accidentally left this in here from the properties file example

--- bootstrap.js
+++ bootstrap.js
@@ -1,8 +1,6 @@
 const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
 Cu.import('resource://gre/modules/Services.jsm');
 
-var stringBundle = Services.strings.createBundle('chrome://l10n/locale/global.properties?' + Math.random()); // Randomize URI to work around bug 719376
-
 function install() {}
 function uninstall() {}
 
