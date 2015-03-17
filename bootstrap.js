044cd029d2afc95781bba8e8ea6f5ef2473e0c47
Noitidart <noitidart@gmail.com>
2015-03-16 21:18:21 -0700

    Update bootstrap.js
    * fixed so origina lreplacements works
    * show demos of what happens if array has less then expected replacements (we see it gets replaced with null)

--- bootstrap.js
+++ bootstrap.js
@@ -9,9 +9,11 @@ function uninstall() {}
 function startup() {
 	Services.prompt.alert(null, stringBundle.GetStringFromName('startup_prompt_title'), stringBundle.GetStringFromName('startup_prompt_message'));
 	var replacements = ['first rep', '2nd replaced', 'and last one replaced'];
-	Services.prompt.alert(null, stringBundle.GetStringFromName('startup_prompt_title'), stringBundle.formatStringFromName('startup_prompt_message', [], [].length));
+	Services.prompt.alert(null, stringBundle.GetStringFromName('startup_prompt_title'), stringBundle.formatStringFromName('msg_with_replacments', replacements, replacements.length));
+	Services.prompt.alert(null, stringBundle.GetStringFromName('startup_prompt_title'), stringBundle.formatStringFromName('msg_with_replacments', [], [].length)); // just demo
 	
-	//Services.prompt.alert(null, stringBundle.GetStringFromName('startup_prompt_title'), stringBundle.formatStringFromName('startup_prompt_message', [], [].length)); //also works
+	Services.prompt.alert(null, stringBundle.GetStringFromName('startup_prompt_title'), stringBundle.formatStringFromName('startup_prompt_message', [], [].length)); //also works
+	Services.prompt.alert(null, stringBundle.GetStringFromName('startup_prompt_title'), stringBundle.formatStringFromName('startup_prompt_message', replacements, replacements.length)); //also works
 }
  
 function shutdown() {}
